580192490f9fdd6578111a852b0c7123f146f32b
ilmo.euro <ilmo.euro@gmail.com>
2017-06-08 13:04:24 +0300

    ** Correct JSON credentials

--- src/app.py
+++ src/app.py
@@ -55,10 +55,10 @@ def get_credentials():
         abort(403)
     user = "{0.first_name}.{0.last_name}".format(current_user)
     jid = user + "@chatproto.muikkuverkko.fi"
-    key = str(get_totp(b32encode(md5(jid).digest())))
+    password = str(get_totp(b32encode(md5(jid).digest())))
     return jsonify({
         "jid": jid,
-        "key": key
+        "password": password
     })
 
 
